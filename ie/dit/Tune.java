c64177f76aed2364701601681c4d26c47127c7c6
Kalyan Kola Cahill <kalyankolacahill@gmail.com>
2018-02-14 16:15:45 +0000

    Tune Class

---
+++ ie/dit/Tune.java
@@ -0,0 +1,23 @@
+package ie.dit;
+
+import java.io.*;
+
+public class Tune {
+	
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+	
+    public String toString()
+    {
+        String s = "";
+		
+		for(String song:)
+        {
+            s.append(song + ", ");
+        }		
+
+        return s.toString();
+    }	
+}
