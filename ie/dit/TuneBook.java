0e6e5777a2825d8c75e0a844e47c3b405e6199fb
Kalyan Kola Cahill <kalyankolacahill@gmail.com>
2018-02-14 16:23:12 +0000

    TuneBook class and toString method

--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -1,1 +1,33 @@
-s
+package ie.dit;
+
+import java.io.*;
+//import java.util.ArrayList;
+
+public class TuneBook {
+
+	ArrayList<String> tunes = new ArrayList<String>();
+
+	public TuneBook(String s){
+		
+		String fileName = s;
+		
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while((line = bufferedReader.readLine()) != null)
+			{
+				tunes.add(line);
+			}
+			
+			bufferedReader.close();
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println("Error opening file");
+		}		
+		catch(IOException ex) {
+			System.out.println("Error reading file" + fileName);
+		}		
+	}
+}
